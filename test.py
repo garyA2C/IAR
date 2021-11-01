e5cc2f40e666d2bdb58bd3428101a1bb06ffc9a9
emmanuel <emmanuel.montblanc@insa-lyon.fr>
2021-11-01 14:51:53 +0100

    test de l'environnement, ca a l'air de fonctionner, mais le render est bloquant

--- test.py
+++ test.py
@@ -2,5 +2,13 @@ import env_homework
 
 env = env_homework.cleanerEnv()
 
-while True:
+obs = env.reset()
+env.render()
+print(obs)
+for _ in range(10):
+    action = 0
+    obs, reward, done, info = env.step(action)
+    print(obs)
+    print(reward)
+    print("")
     env.render()
